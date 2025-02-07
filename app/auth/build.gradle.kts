94c59f6e166b6ca93884a05cbc4fe675cf239d77
Simon Duchastel <simon@duchastel.com>
2025-02-06 23:24:09 -0500

    Remove unneeded deps and unused class

--- app/auth/build.gradle.kts
+++ app/auth/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -31,17 +30,12 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures {
-        compose = true
-    }
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.material3)
     implementation(libs.okhttp)
-    implementation(libs.coil.compose)
 
     testImplementation(libs.junit)
 }
